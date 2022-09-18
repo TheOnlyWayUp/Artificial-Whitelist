95955900cd05684ae3f36e5c36ace5786f55fecb
TheOnlyWayUp <thedarkdraconian@gmail.com>
2022-09-18 16:55:51 +0000

    Add StatsAPI Files

---
+++ src/StatsAPI/src/main.py
@@ -0,0 +1,78 @@
+import os, json
+from typing import Dict, List
+from fastapi import FastAPI, Header, Depends
+from fastapi.exceptions import HTTPException
+
+# --- Parsing Config --- #
+
+STATS_API_CONFIG_PATH = os.getenv("STATS_API_CONFIG_PATH")
+assert (
+    STATS_API_CONFIG_PATH
+), "Set the STATS_API_CONFIG_PATH Environment Variable before running."
+with open(STATS_API_CONFIG_PATH) as config_file:
+    config = json.load(config_file)
+
+BIND_ADDRESS = config["bind"]["address"]
+BIND_PORT = config["bind"]["port"]
+
+DATABASE_PATH = config["db_file"]
+AUTHORIZATION = config["authorization"]
+
+
+def check_auth(authorization: str = Header(...)):
+    if authorization != AUTHORIZATION:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+
+app = FastAPI(dependencies=[Depends(check_auth)])
+
+try:
+    with open(DATABASE_PATH, "r") as handler:
+        ...
+except:
+    with open(DATABASE_PATH, "w") as handler:
+        json.dump({}, handler)
+
+# --- #
+
+
+@app.get("/log/{username}/{address}")
+def log(username: str, address: str):
+    with open(DATABASE_PATH) as handler:
+        old_data: Dict[str, List[str]] = json.load(
+            handler
+        )  # usernames: list of ip addresses
+    data = set(old_data.get(username, []))
+    data.add(address)
+    data = list(data)
+    old_data[username] = data
+    with open(DATABASE_PATH, "w") as handler:
+        json.dump(old_data, handler)
+    return 200
+
+
+@app.get("/similar/{username}")
+def similar(username: str):
+    with open(DATABASE_PATH) as handler:
+        old_data: Dict[str, List[str]] = json.load(
+            handler
+        )  # usernames: list of ip addresses
+    new_data: Dict[str, List[str]] = {}  # ip_address, list of usernames
+    for username, addresses in old_data.items():
+        for ip_address in addresses:
+            d = set(new_data.get(ip_address, []))
+            d.add(username)
+            new_data[ip_address] = list(d)
+    to_return = set()
+    for ip_address in old_data[username]:
+        for other_user in new_data[ip_address]:
+            to_return.add(other_user)
+    return list(to_return)
+
+
+# --- #
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host=BIND_ADDRESS, port=BIND_PORT)
